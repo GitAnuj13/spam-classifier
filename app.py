a26a6de39cfdae0fdc998f75326255aabeb618cb
Anuj Upadhyay <134092817+GitAnuj13@users.noreply.github.com>
2023-07-06 20:53:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,21 +24,21 @@ def transform_text(text):
         y.append(ps.stem(i))
 
     return " ".join(y)
-    tfidf=pickle.load(open('vectorizer.pkl','rb'))
-    model=pickle.load(open('model.pkl','rb'))
-    st.title("Email/SMS classifier")
-    input_sms=st.text_area("Enter the message")
-    if st.button('Predict'):
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('model.pkl','rb'))
+st.title("Email/SMS classifier")
+input_sms=st.text_area("Enter the message")
+if st.button('Predict'):
 
-        # 1. preprocess
-        transformed_sms = transform_text(input_sms)
-        # 2. vectorize
-        vector_input = tfidf.transform([transformed_sms])
-        # 3. predict
-        result = model.predict(vector_input)[0]
-        # 4. Display
-        if result == 1:
-            st.header("Spam")
-        else:
-            st.header("Not Spam")
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
         
